0810bdfed170baa4909a86ced40b55f2a764e7fa
yermakhanuly <ernarermakhanuly@gmail.com>
2025-09-04 14:36:23 +0800

    adding env variable

--- frontend/src/config/axios.ts
+++ frontend/src/config/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_URL = process.env.REACT_APP_BACKEND_UR || 'http://localhost:5000/api';
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
